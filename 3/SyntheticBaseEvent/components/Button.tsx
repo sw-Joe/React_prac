2da6898458e01c2a6d08dfea8ae58937a443aa32
joe-laptop <user.aomurasaki@gmail.com>
2026-03-31 15:11:29 +0900

    dir reorganized

--- 3/SyntheticBaseEvent/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-export default function Button({handleClick}: {
-    handleClick: (evnet: React.MouseEvent<HTMLButtonElement>) => void;
-    }) {
-    return (
-        <>
-            <button onClick={handleClick}>click here</button>
-        </>
-    )
-}
--- 3_eventHandler/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-export default function Button(props: {msg: string; children: React.ReactNode}) {
-    const clickHandler = () => { console.debug(props.msg) };
-
-    return (
-        <>
-            <button onClick={clickHandler}>{props.children}</button>
-        </>
-    )
-}
